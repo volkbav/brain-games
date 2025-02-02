e9c8f160ddb140410696ddaaf7d1de0fcade89c8
volkbav <berezov86@gmail.com>
2025-02-02 14:32:00 +0300

    fix linter

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,6 +1,4 @@
 from brain_games.games import logic_prime
-
-
 from brain_games.games.my_functions import game
 
 
--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,6 +1,7 @@
 from brain_games.games import logic_progression
 from brain_games.games.my_functions import game
 
+
 def main():
     game(logic_progression)
 
